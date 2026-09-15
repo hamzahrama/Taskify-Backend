3ccc29c054d005dad28c48336e2bf360b0460e2a
hamzahrama <hamzahrama@gmail.com>
2026-09-15 14:41:38 +0700

    fix: configure port 4000, cookie-parser, validation pipe, cors

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,18 @@ import { AuthModule } from './auth/auth.module.js';
 import { BoardsModule } from './boards/boards.module.js';
 import { ColumnsModule } from './columns/columns.module.js';
 import { TasksModule } from './tasks/tasks.module.js';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule, UsersModule, AuthModule, BoardsModule, ColumnsModule, TasksModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }), // tambahkan ini paling atas
+    PrismaModule,
+    AuthModule,
+    UsersModule,
+    BoardsModule,
+    ColumnsModule,
+    TasksModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
